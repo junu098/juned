333efc1f0b0aad1c4b50e27e2d13113d3cf49ec6
mrnick82 <mrnick8250@gmail.com>
2025-05-19 21:20:10 +0530

    Update autoleave.py

--- AudifyMusic/plugins/misc/autoleave.py
+++ AudifyMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1002348455705
+                                and i.chat.id != -1002348455705
                             ):
                                 if left == 20:
                                     continue
